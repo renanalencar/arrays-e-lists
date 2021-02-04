3cbd48f3b39c286640b251ae624c34a4932f0a0d
Renan Costa Alencar <renan.costaalencar@gmail.com>
2021-02-04 17:52:44 -0300

    Inclusão de polimorfismo na impressão

--- src/fundamentos/Sistema.java
+++ src/fundamentos/Sistema.java
@@ -14,8 +14,9 @@ import java.util.List;
 public class Sistema {
 
 	/***
-	 * Método principal da classe para realizar testes na conversão entre
-	 * listas e arrays e vice-versa.
+	 * Método principal da classe para realizar testes na conversão entre listas e
+	 * arrays e vice-versa.
+	 * 
 	 * @param args String[]
 	 */
 	public static void main(String[] args) {
@@ -30,11 +31,11 @@ public class Sistema {
 		arrPessoas[1] = "Wesley";
 		arrPessoas[2] = "Guilherme";
 		arrPessoas[3] = "Renam";
-		
+
 		System.out.println("Lista de valores utilizando Array:");
-		
+
 		imprimirConteudo(arrPessoas);
-		
+
 		// Para atribuir valores para cada posição do array, precisamos acessar a
 		// posição utilizando o índice e declarar o que ele contém.
 
@@ -46,12 +47,10 @@ public class Sistema {
 		// contém um método próprio de transformar array para lista, o asList.
 
 		lisPessoas = Arrays.asList(arrPessoas);
-		
-		System.out.println("\nLista de valores List utilizando método da classe Arrays:");
 
-		lisPessoas.forEach(nome -> imprimirValor(nome));
+		System.out.println("\nLista de valores List utilizando método da classe Arrays:");
 		
-//		imprimirConteudo(lisPessoas);
+		imprimirConteudo(lisPessoas);
 
 		/*
 		 * Podemos também fazer da forma mais básica, iterando em cima do array e
@@ -62,31 +61,31 @@ public class Sistema {
 		for (String nome : arrPessoas) {
 			lisPessoas.add(nome);
 		}
-		
+
 		System.out.println("\nLista de valores utilizando estrutura de repetição para preencher a lista:");
-		
-		lisPessoas.forEach(nome -> imprimirValor(nome));
-		
-//		imprimirConteudo(lisPessoas);
+
+		imprimirConteudo(lisPessoas);
 
 		// 2- Transformando listas em arrays
 
 		/*
-		 * Para transformar uma lista em um array podemos utilizar um método da interface List e
-		 * implementado por ArrayList, que é a classe base. Além disso esse método retorna normalmente um
-		 * array de objetos; portanto precisamos passar como parêmetro o tipo de retorno
-		 * desejado para que ele realize a conversão.
+		 * Para transformar uma lista em um array podemos utilizar um método da
+		 * interface List e implementado por ArrayList, que é a classe base. Além disso
+		 * esse método retorna normalmente um array de objetos; portanto precisamos
+		 * passar como parêmetro o tipo de retorno desejado para que ele realize a
+		 * conversão.
 		 */
 		arrPessoas = lisPessoas.toArray(new String[lisPessoas.size()]);
-		
+
 		System.out.println("\nLista de valores utilizando método da classe Collection para transformar em array:");
-		
+
 		imprimirConteudo(arrPessoas);
 
 		/*
 		 * Uma outra forma de transformar uma lista em um array seria fazer uma iteração
 		 * na mesma para adicionar cada elemento no array. Para inicializar o array
-		 * precisaremos utilizar o tamanho total da lista, pois o array tem tamanho fixo.
+		 * precisaremos utilizar o tamanho total da lista, pois o array tem tamanho
+		 * fixo.
 		 */
 
 		arrPessoas = new String[lisPessoas.size()];
@@ -94,25 +93,17 @@ public class Sistema {
 		for (int i = 0; i < lisPessoas.size(); i++) {
 			arrPessoas[i] = lisPessoas.get(i);
 		}
-		
+
 		System.out.println("\nLista de valores utilizando estrutura de repetição para preencher o Array:");
-		
+
 		imprimirConteudo(arrPessoas);
 
 	}
-	
+
 	/***
-	 * Método para encurtar a linha de comando para impressão.
-	 * Imprime um único valor.
-	 * @param s String
-	 */
-	public static void imprimirValor(String s) {
-		System.out.println(s);
-	}
-	
-	/***
-	 * Método para imprimir todo o conteúdo de um array.
-	 * Recebe como parametro um array de strings.
+	 * Método para imprimir todo o conteúdo de um array. Recebe como parametro um
+	 * array de strings.
+	 * 
 	 * @param arr String[]
 	 */
 	public static void imprimirConteudo(String[] arr) {
@@ -120,16 +111,15 @@ public class Sistema {
 			System.out.println(s);
 		}
 	}
-	
+
 	/***
-	 * Método para imprimir todo conteúdo de uma lista.
-	 * Recebe como parametro um objeto do tipo List.
+	 * Método para imprimir todo conteúdo de uma lista. Recebe como parametro um
+	 * objeto do tipo List.
+	 * 
 	 * @param l List
 	 */
 	public static void imprimirConteudo(List<String> l) {
-		for (String s : l) {
-			System.out.println(s);
-		}
+		l.forEach(s -> System.out.println(s));
 	}
 
 }
